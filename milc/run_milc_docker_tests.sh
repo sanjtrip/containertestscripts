e57bb3c726a9cc24943e627db00efe38e493014c
sidamd <Sid.srinivasan@amd.com>
2021-07-19 09:48:06 -0700

    V1.0 script

--- milc/run_milc_docker_tests.sh
+++ milc/run_milc_docker_tests.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
+#
+#Copyright (c) 2021 Advanced Micro Devices, Inc. All Rights Reserved.
+#Author: Siddharth Srinivasan, Sid.srinivasan@amd.com
+#Version: V1.0
+#
+#This script runs the MILC test commands for docker container
+#
+#To run the script, download the script using wget then:
+#	sh ./run_milc_docker_tests.sh
+#
 
-# Copyright (c) 2021 Advanced Micro Devices, Inc. All Rights Reserved.
-
+echo "Pull amdih/milc docker container"
 docker pull amdih/milc:12ddd7d9
+echo "Pull complete"
 
+echo "Create an out directory"
 mkdir -p out
 
-docker run --rm -it --device /dev/dri --device /dev/kfd --security-opt seccomp=unconfined amdih/milc:12ddd7d9 /bin/bash -c "cd /benchmark; run-benchmark -o milc-benchmark.out"
+echo "Run the milc docker and run the run-benchmark script and output to milc-benchmark.out"
+docker run --rm -it --device /dev/dri --device /dev/kfd -v `pwd`/out:/benchmark/out --security-opt seccomp=unconfined amdih/milc:12ddd7d9 /bin/bash -c "cd /benchmark; run-benchmark -o milc-benchmark.out"
 
