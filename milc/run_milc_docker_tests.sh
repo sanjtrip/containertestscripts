fc94b047e72e13564d83c8bf931cb2798d9212f4
sidamd <Sid.srinivasan@amd.com>
2021-07-20 11:19:25 -0700

    V1.1 scripts

--- milc/run_milc_docker_tests.sh
+++ milc/run_milc_docker_tests.sh
@@ -14,6 +14,26 @@ echo "Pull amdih/milc docker container"
 docker pull amdih/milc:12ddd7d9
 echo "Pull complete"
 
+echo "===== Section: Available ROCm versions ==============="
+/bin/ls -v -d /opt/rocm*
+ROCM_VERSION=`/bin/ls -v -d /opt/rocm-[3-4]* | /usr/bin/tail -1`
+if [ "$ROCM_VERSION"x = "x" ]
+then
+    ROCM_VERSION=`/bin/ls -v -d /opt/rocm* | /usr/bin/tail -1`
+fi
+echo "==== Using $ROCM_VERSION to collect ROCm information.==== "
+$ROCM_VERSION/bin/rocm-smi
+$ROCM_VERSION/bin/rocm-smi --showhw
+
+
 echo "Run the milc docker and run the run-benchmark script and output to milc-benchmark.out"
 docker run --rm -it --device /dev/dri --device /dev/kfd -v `pwd`/out:/benchmark/out --security-opt seccomp=unconfined amdih/milc:12ddd7d9 /bin/bash -c "cd /benchmark; run-benchmark -o milc-benchmark.out"
 echo "milc run-benchmark script complete"
+
+
+echo "==== Using $ROCM_VERSION to collect ROCm information.==== "
+$ROCM_VERSION/bin/rocm-smi
+$ROCM_VERSION/bin/rocm-smi --showhw
+
+echo "Show logs"
+/bin/dmesg | grep -E 'gpu|pcie' | tail -100
