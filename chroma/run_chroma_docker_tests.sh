2ddedddc8762035a8134143327deea6110181716
sidamd <Sid.srinivasan@amd.com>
2021-07-21 10:28:30 -0700

    Docker V2.0

--- chroma/run_chroma_docker_tests.sh
+++ chroma/run_chroma_docker_tests.sh
@@ -2,7 +2,7 @@
 #
 #Copyright (c) 2021 Advanced Micro Devices, Inc. All Rights Reserved.
 #Author: Siddharth Srinivasan, Sid.srinivasan@amd.com
-#Version: V1.0
+#Version: V2.0
 #
 #This script runs the Chroma test commands for docker container
 #
@@ -25,8 +25,20 @@ echo "==== Using $ROCM_VERSION to collect ROCm information.==== "
 $ROCM_VERSION/bin/rocm-smi
 $ROCM_VERSION/bin/rocm-smi --showhw
 
-echo "Run the chroma docker and run the run-benchmark script with the option --ngpus 1
-docker run --rm -it --device /dev/dri --device /dev/kfd --security-opt seccomp=unconfined amdih/chroma:3.43.0 /bin/bash -c "cd /benchmark; run-benchmark --ngpus 1"
+echo "Run the chroma docker and run the run-benchmark script with the option --ngpus 1"
+docker run --rm -it --device /dev/dri --device /dev/kfd --security-opt seccomp=unconfined amdih/chroma:3.43.0 /bin/bash -c "cd /benchmark; run-benchmark --ngpus 1; "
+echo "chroma run-benchmark script complete"
+
+echo "Run the chroma docker and run the run-benchmark script with the option --ngpus 2"
+docker run --rm -it --device /dev/dri --device /dev/kfd --security-opt seccomp=unconfined amdih/chroma:3.43.0 /bin/bash -c "cd /benchmark; run-benchmark --ngpus 2; "
+echo "chroma run-benchmark script complete"
+
+echo "Run the chroma docker and run the run-benchmark script with the option --ngpus 4"
+docker run --rm -it --device /dev/dri --device /dev/kfd --security-opt seccomp=unconfined amdih/chroma:3.43.0 /bin/bash -c "cd /benchmark; run-benchmark --ngpus 4; "
+echo "chroma run-benchmark script complete"
+
+echo "Run the chroma docker and run the run-benchmark script with the option --ngpus 8"
+docker run --rm -it --device /dev/dri --device /dev/kfd --security-opt seccomp=unconfined amdih/chroma:3.43.0 /bin/bash -c "cd /benchmark; run-benchmark --ngpus 8; "
 echo "chroma run-benchmark script complete"
 
 echo "==== Using $ROCM_VERSION to collect ROCm information.==== "
